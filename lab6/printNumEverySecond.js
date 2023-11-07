4a15232265a9d752ded819e335b92c3f2dbcac7c
aakarshan <aakartionsimkhada@gmail.com>
2023-11-07 14:26:14 -0600

    Finish lab 6

--- lab6/printNumEverySecond.js
+++ lab6/printNumEverySecond.js
@@ -1,1 +1,14 @@
 // Add numbers every seconds
+function printNumbers(from, to){
+    
+    const timerId = setInterval(()=>{
+        if(from > to){
+            clearInterval(timerId);
+        }
+        else{
+            console.log(from++);
+        }
+    }, 1000)
+}
+
+printNumbers(3,10);
